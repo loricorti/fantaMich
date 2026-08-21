090edd3c4a63bfb154e4ae6e0a3a1b0125c45d82
Lorenzo Corti <lorenzo.corti@finconsgroup.com>
2026-08-21 16:43:39 +0200

    MandaOfferta button

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,66 @@
 import { useEffect, useState } from 'react'
 import { supabase } from './supabase'
 
-const names = [
-  'Mario',
-  'Luca',
-  'Stefano',
-  'Andrea',
+const calciatori: string[] = [
+  "Lautaro Martinez (INT)",
+  "Nicolò Barella (INT)",
+  "Alessandro Bastoni (INT)",
+  "Federico Dimarco (INT)",
+  "Marcus Thuram (INT)",
+  "Hakan Calhanoglu (INT)",
+  "Mike Maignan (MIL)",
+  "Theo Hernandez (MIL)",
+  "Rafael Leao (MIL)",
+  "Christian Pulisic (MIL)",
+  "Tijjani Reijnders (MIL)",
+  "Fikayo Tomori (MIL)",
+  "Paulo Dybala (ROM)",
+  "Mats Hummels (ROM)",
+  "Artem Dovbyk (ROM)",
+  "Lorenzo Pellegrini (ROM)",
+  "Gianluca Mancini (ROM)",
+  "Bryan Cristante (ROM)",
+  "Dušan Vlahović (JUV)",
+  "Kenan Yildiz (JUV)",
+  "Federico Chiesa (JUV)",
+  "Teun Koopmeiners (JUV)",
+  "Manuel Locatelli (JUV)",
+  "Bremer (JUV)",
+  "Rasmus Højlund (ATA)",
+  "Ademola Lookman (ATA)",
+  "Charles De Ketelaere (ATA)",
+  "Ederson (ATA)",
+  "Marten de Roon (ATA)",
+  "Gianluca Scamacca (ATA)",
+  "Riccardo Orsolini (BOL)",
+  "Lewis Ferguson (BOL)",
+  "Dan Ndoye (BOL)",
+  "Remo Freuler (BOL)",
+  "Joshua Zirkzee (BOL)",
+  "Khvicha Kvaratskhelia (NAP)",
+  "Romelu Lukaku (NAP)",
+  "Matteo Politano (NAP)",
+  "Stanislav Lobotka (NAP)",
+  "Giovanni Di Lorenzo (NAP)",
+  "Nico Gonzalez (FIO)",
+  "Moise Kean (FIO)",
+  "Robin Gosens (FIO)",
+  "Lucas Beltran (FIO)",
+  "Albert Gudmundsson (FIO)",
+  "Domenico Berardi (SAS)",
+  "Andrea Pinamonti (SAS)",
+  "Armand Laurienté (SAS)",
 ]
 
 function App() {
 
   const [selectedName, setSelectedName] = useState('')
   const [number, setNumber] = useState(0)
-  const [locked, setLocked] = useState(false)
+  const [localNumber, setLocalNumber] = useState(0)
+  const [fuoriDallAsta, setLocked] = useState(false)
   const [searchText, setSearchText] = useState('')
   const [showSuggestions, setShowSuggestions] = useState(false)
-  const filteredNames = names
+  const filteredNames = calciatori
     .filter(name =>
       name.toLowerCase().includes(searchText.toLowerCase())
     )
@@ -46,6 +91,7 @@ function App() {
       setSelectedName(data.selected_name)
       setNumber(data.number_value)
       setLocked(data.locked)
+      setLocalNumber(0)
     }
 
     loadState()
@@ -92,6 +138,7 @@ function App() {
   async function handleNameChange(name: string) {
 
     setSelectedName(name)
+    setLocalNumber(0)
     setSearchText(name)
     setNumber(0)
     setLocked(false)
@@ -132,20 +179,21 @@ function App() {
     }
   }
 
-
-  // Cambio numero
+  // Cambio locale numero
   async function handleNumberChange(
     event: React.ChangeEvent<HTMLInputElement>
   ) {
-
     const value = Number(event.target.value)
+    setLocalNumber(value)
+  }
 
-    setNumber(value)
-
+  // Cambio offerta database
+  async function handleSendOffer(offer: number) {
+    setNumber(offer)
     const { error } = await supabase
       .from('shared_state')
       .update({
-        number_value: value,
+        number_value: offer,
       })
       .eq('id', 1)
 
@@ -154,10 +202,9 @@ function App() {
     }
   }
 
-
   // Conferma
-  async function handleConfirm() {
-
+  async function handleExit() {
+    setLocalNumber(0)
     const { error } = await supabase
       .from('shared_state')
       .update({
@@ -178,26 +225,26 @@ function App() {
         minHeight: '100vh',
         display: 'flex',
         justifyContent: 'center',
+        backgroundColor: '#f6d09a',
       }}>
 
       <div style={{
-        maxWidth: '800px',
+        maxWidth: '1200px',
         marginLeft: '50px',
         marginRight: '50px',
         paddingLeft: '60px',
         paddingRight: '60px',
         paddingTop: '20px',
         paddingBottom: '20px',
-        backgroundColor: '#9af6c0',
       }}>
 
-        <h1 style={{ marginBottom: '60px' }}>Asta di Michelangelo Tentori</h1>
-
-        <div style={{ marginBottom: '60px' }}>
+        <div style={{
+          marginBottom: '100px',
+          marginTop: '30px',
+        }}>
 
           <label style={{
-            fontSize: '30px',
-            marginBottom: '30px',
+            fontSize: '40px',
           }}>
             Giocatore
           </label>
@@ -209,7 +256,7 @@ function App() {
               style={{
                 position: 'relative',
                 width: '100%',
-                marginTop: '5px',
+                marginTop: '20px',
               }}
             >
               <input
@@ -229,8 +276,8 @@ function App() {
                   boxSizing: 'border-box',
                   border: '1px solid #cccccc',
                   borderRadius: '6px',
-                  height: '50px',
-                  fontSize: '22px',
+                  height: '80px',
+                  fontSize: '60px',
                   textAlign: 'center',
                 }}
               />
@@ -299,9 +346,10 @@ function App() {
               style={{
                 margin: '5px 0 0 0',
                 padding: '10px',
-                fontSize: '40px',
+                fontSize: '60px',
                 color: '#000000',
                 fontWeight: 'bold',
+                marginTop: '20px',
               }}
             >
               {selectedName}
@@ -310,72 +358,142 @@ function App() {
         </div>
 
 
-        <div style={{ marginBottom: '20px' }}>
-
-          <label style={{
-            fontSize: '30px',
-            marginBottom: '20px',
-          }}>
-            Valore offerto
-          </label>
+        <div style={{ marginBottom: '40px' }}>
 
           {isUserA ? (
-            <label style={{
-              fontSize: '30px',
-              marginLeft: '20px',
-            }}>
-              {number}M
-            </label>
-          ) : (
             <div>
-              <br />
+              <label style={{
+                fontSize: '60px',
+              }}>
+                Offerta Mich:
+              </label>
 
-              <input
-                type="number"
-                value={number}
-                disabled={!isUserB || locked}
-                onChange={handleNumberChange}
+              <label style={{
+                fontSize: '60px',
+                marginLeft: '30px',
+              }}>
+                {number === 0 || fuoriDallAsta ? '-' : number}
+              </label>
+            </div>
+          ) : (
+            <div
+              style={{
+                display: 'flex',
+                flexDirection: 'row',
+                justifyContent: 'center',
+                gap: '50px',
+              }}>
+              <div
                 style={{
-                  width: '100.px',
-                  backgroundColor: locked ? '#3a3a3a' : '#ffffff',
-                  color: locked ? '#ffffff' : '#000000',
-                  border: locked ? '1px solid #555555' : '1px solid #cccccc',
-                  borderRadius: '6px',
-                  cursor: locked ? 'not-allowed' : 'text',
-                  opacity: 1,
-                  height: '50px',
-                  fontSize: '22px',
-                  textAlign: 'center'
-                }}
+                  width: '30%',
+                  display: 'flex',
+                  flexDirection: 'column',
+                  gap: '30px',
+                }}>
+                <label style={{
+                  fontSize: '60px',
+                  lineHeight: '1.1',
+                }}>
+                  Tua ultima offerta:
+                </label>
 
-              />
+                <label style={{
+                  fontSize: '60px',
+                  marginLeft: '30px',
+                }}>
+                  {number === 0 || fuoriDallAsta ? '-' : number}
+                </label>
+              </div>
+
+              <div
+                style={{
+                  width: '30%',
+                  display: 'flex',
+                  flexDirection: 'column',
+                  gap: '30px',
+                }}>
+                <label style={{
+                  fontSize: '60px',
+                  lineHeight: '1.1',
+                }}>
+                  Nuova offerta:
+                </label>
+
+                <input
+                  type="number"
+                  value={localNumber}
+                  disabled={fuoriDallAsta}
+                  onChange={handleNumberChange}
+                  style={{
+                    width: '100.px',
+                    backgroundColor: fuoriDallAsta ? '#3a3a3a' : '#ffffff',
+                    color: fuoriDallAsta ? '#ffffff' : '#000000',
+                    border: fuoriDallAsta ? '1px solid #555555' : '1px solid #cccccc',
+                    borderRadius: '6px',
+                    cursor: fuoriDallAsta ? 'not-allowed' : 'text',
+                    opacity: 1,
+                    height: '50px',
+                    fontSize: '30px',
+                    textAlign: 'center'
+                  }}
+                />
+
+              </div>
+
+              <div
+                style={{
+                  width: '30%',
+                  display: 'flex',
+                  flexDirection: 'column',
+                  alignItems: 'center',
+                  gap: '30px',
+                }}>
+                <button
+                  onClick={
+                    () => handleSendOffer(localNumber)
+                  }
+                  disabled={!isUserB || fuoriDallAsta}
+                  style={{
+                    height: 'fit-content',
+                    width: 'fit-content',
+                    padding: '10px 20px',
+                    fontSize: '40px',
+                    marginTop: '20px',
+                  }}
+                >
+                  Manda Offerta
+                </button>
+
+                <button
+                  onClick={handleExit}
+                  disabled={!isUserB || fuoriDallAsta}
+                  style={{
+                    height: 'fit-content',
+                    width: 'fit-content',
+                    padding: '10px 20px',
+                    fontSize: '40px',
+                    marginTop: '20px',
+                  }}
+                >
+                  Esci dall'Asta
+                </button>
+              </div>
+
+
             </div>
           )}
         </div>
 
-        {isUserB && (
-          <button
-            onClick={handleConfirm}
-            disabled={!isUserB || locked}
-            style={{
-              height: '60px',
-              width: '100px',
-              fontSize: '20px',
-              marginTop: '20px',
-            }}
-          >
-            FUORI
-          </button>
-        )}
-
-
-        {locked && (
+        {fuoriDallAsta && (
           <p style={{
             marginTop: '30px',
             fontWeight: 'bold',
-            fontSize: '30px',
+            fontSize: '50px',
           }}>
-            Mich Tentori è fuori dall'asta per {selectedName}.
+            {isUserA ?
+              `Mich è fuori dall'asta.`
+              : `Sei fuori dall'asta per ${selectedName}.`
+            }
           </p>
         )}
 
@@ -383,15 +501,15 @@ function App() {
           style={{
             marginTop: '40px',
             display: 'flex',
-            gap: '20px',
-            alignItems: 'center',
+            gap: '100px',
+            justifyContent: 'center',
           }}
         >
           <img
             src="/robin_hood.jpeg"
             alt=""
             style={{
-              width: '50%',
+              width: '40%',
               maxWidth: '600px',
               height: 'auto',
               borderRadius: '8px',
@@ -402,14 +520,13 @@ function App() {
             src="/squadra_ginew.jpeg"
             alt=""
             style={{
-              width: '50%',
+              width: '40%',
               maxWidth: '600px',
               height: 'auto',
               borderRadius: '8px',
             }}
           />
         </div>
-
       </div>
     </div>
   )
